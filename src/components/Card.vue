aa133ffbcce04872bca5be8c2628191ec0f79bf2
Mr.Blue <silverhugh.77@gmail.com>
2021-06-26 17:15:56 +0800

    update source

---
+++ src/components/Card.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="w-48 mb-5">
+    <router-link :to="route">
+      <img
+        :src="require('@/assets/' + screenshot)"
+        class="shadow-xl rounded transition duration-150 ease-in-out transform hover:scale-105"
+      />
+      <div class="mt-2"><slot></slot></div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    route: {
+      type: String,
+    },
+    screenshot: {
+      type: String,
+    },
+    text: {
+      type: String,
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,7 @@
 <template>
   <div class="bg-gray-800 min-h-screen text-white">
     <div>
-      <!-- Place this tag where you want the button to render. -->
-    </div>
-    <div class="pt-10 text-center">
-      <div class="flex justify-center items-end">
-        <p class="text-5xl mr-2">Tactical Board</p>
+      <div class="absolute top-5 right-5">
         <github-button
           href="https://github.com/tacticalboard/tacticalboard"
           data-show-count="true"
@@ -13,34 +9,25 @@
           >Star</github-button
         >
       </div>
-      <p class="mt-1 text-xl">Make tactical communication convenient.</p>
+      <div class="absolute mx-auto bottom-5 left-0 right-0 text-center">
+        <p class="text-sm text-gray-400">
+          Developed by <a href="https://github.com/hughplay">@hughplay</a>
+        </p>
+      </div>
+    </div>
+    <div class="pt-10 text-center">
+      <p class="text-5xl mr-2">Tactical Board</p>
+      <p class="mt-6 text-xl text-gray-400">Make tactical communication convenient.</p>
     </div>
     <div class="px-2 mx-auto max-w-3xl mt-16">
       <div class="flex items-end justify-around text-center flex-wrap">
-        <div class="w-48 mb-5">
-          <router-link to="/shuttle">
-            <img src="@/assets/screenshot/shuttle.jpg" alt="" />
-            <p class="mt-2">Badminton</p>
-          </router-link>
-        </div>
-
-        <div class="w-48 mb-5">
-          <router-link to="/football">
-            <img src="@/assets/screenshot/football.jpg" alt="" />
-            <p class="mt-2">Football</p>
-          </router-link>
-        </div>
-
-        <div class="w-48 mb-5">
-          <router-link to="/basketball">
-            <img src="@/assets/screenshot/basketball.jpg" alt="" />
-            <p class="mt-2">Basketball</p>
-          </router-link>
-        </div>
+        <card route="/shuttle" screenshot="screenshot/shuttle.jpg">Badminton</card>
+        <card route="/football" screenshot="screenshot/football.jpg">Football</card>
+        <card route="/basketball" screenshot="screenshot/basketball.jpg">Basketball</card>
       </div>
     </div>
     <div>
-      <p class="text-center text-xs mt-5">
+      <p class="text-center text-xs mt-5 text-gray-400">
         *Double click the ball to enter/exit the full screen mode.
       </p>
     </div>
@@ -51,11 +38,13 @@
 // @ is an alias to /src
 import { useHead } from "@vueuse/head";
 import GithubButton from "vue-github-button";
+import Card from "../components/Card.vue"
 
 export default {
   name: "Home",
   components: {
     GithubButton,
+    Card
   },
   setup: function () {
     useHead({
